7d0835dd17f0c3c7af05d709ee7c9c759410767b
Louis Celeyron <50552167+LouisViktorCeleyron@users.noreply.github.com>
2024-08-02 23:35:41 +0200

    Avant un gros Test
    Lourd Teste en persp je vais Brancher je crois

---
+++ Assets/Scripts/Status/BulkUp.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulkUp : OnStatusStatus
+{
+
+    protected override void OnStatusInflictedAction(Status statusInflicted)
+    {
+        base.OnStatusInflictedAction(statusInflicted);
+    }
+
+
+}
